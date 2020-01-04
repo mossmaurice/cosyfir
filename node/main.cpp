4182972d1ac40dfe6046ae8f46a82852ff5d118e
Simon Hoinkis <simon@hoinkis.info>
2020-01-04 01:53:41 +0100

    Add custom clang-format

--- node/main.cpp
+++ node/main.cpp
@@ -1,18 +1,18 @@
 void _exit(void)
 {
-    while(1)
-	{
+    while (1)
+    {
         // Loop until reset
     }
 }
 
 int main()
 {
-	while(true)
-	{
-		//toggleLed();
-		// Write hello world to USART
-		//sleep(1000);		
-	}
-	return 0;
+    while (true)
+    {
+        // toggleLed();
+        // Write hello world to USART
+        // sleep(1000);
+    }
+    return 0;
 }
