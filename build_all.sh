9f2f9473e0818c2b86cf54abb18ac3ecadae762b
Simon Hoinkis <simon@hoinkis.info>
2020-04-14 00:33:55 +0200

    Add debug mode to build script

--- build_all.sh
+++ build_all.sh
@@ -2,20 +2,23 @@
 
 set -e
 WORKSPACE=$(git rev-parse --show-toplevel)
-# Possible build arguments are "clean"
+# Possible build arguments are "clean", "debug"
 BUILD_ARGS=${1}
 
 if [ $BUILD_ARGS = "clean" ]
 then
     rm -rf node/build
     rm -rf server/build
+elif [ $BUILD_ARGS = "debug" ]
+then
+    DEBUG="-DCMAKE_BUILD_TYPE=Debug"
 fi
 
 echo "~~~~~ Start building CoSyfIr daemon ~~~~~"
 cd server
 mkdir -p build
 cd build
-cmake ..
+cmake $DEBUG ..
 make -j8
 cd $WORKSPACE
 
@@ -23,6 +26,6 @@ echo "~~~~~ Start building CoSyfIr sensor node ~~~~~"
 cd node
 mkdir -p build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE="LoRaMac-node/cmake/toolchain-arm-none-eabi.cmake" ..
+cmake $DEBUG -DCMAKE_TOOLCHAIN_FILE="LoRaMac-node/cmake/toolchain-arm-none-eabi.cmake" ..
 make -j8
 cd $WORKSPACE
